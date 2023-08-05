90cd514359943a2192b63b216fbfd57dc40fe232
khasmir04 <kcaluscusao@gmail.com>
2023-08-05 19:47:51 +0800

    feat: revamped profile

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // content: ["./src/**/*.{html,js}"],
+  // content should not be in src folder since that folder is not used here
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
